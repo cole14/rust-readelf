2791999b6036597b68669fe59291e63809352cf4
Christopher Cole <luna@spacecat.art>
2022-10-10 22:05:20 -0700

    Use the elf::File::open_stream interface instead of open_path
    I think it was a mistake to muddy up the elf interface with helper methods like open_path
    which do really simple std library stuff like opening files. I want to eventually remove
    open_path from the public interface of the elf crate, so switch this over to open_stream instead.

--- src/rust-readelf.rs
+++ src/rust-readelf.rs
@@ -24,23 +24,28 @@ fn main() {
     let args = Args::parse();
 
     let path: PathBuf = From::from(args.file_name);
-    let file = match elf::File::open_path(&path) {
+    let mut io = match std::fs::File::open(path) {
+        Ok(f) => f,
+        Err(e) => panic!("Error: {:?}", e),
+    };
+
+    let elf_file = match elf::File::open_stream(&mut io) {
         Ok(f) => f,
         Err(e) => panic!("Error: {:?}", e),
     };
 
     if args.file_header {
-        println!("{}", file.ehdr);
+        println!("{}", elf_file.ehdr);
     }
 
     if args.program_headers {
-        for phdr in file.phdrs {
+        for phdr in elf_file.phdrs {
             println!("{}", phdr);
         }
     }
 
     if args.section_headers {
-        for s in file.sections {
+        for s in elf_file.sections {
             println!("{}", s.shdr);
         }
     }
