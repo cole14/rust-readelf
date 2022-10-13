a15412113f02489fea442f950a08d7592fce2a9e
Christopher Cole <luna@spacecat.art>
2022-10-12 23:15:39 -0700

    Add --symbols and --dynamic-symbols

--- src/rust-readelf.rs
+++ src/rust-readelf.rs
@@ -1,23 +1,29 @@
 extern crate clap;
 extern crate elf;
 
-use std::path::PathBuf;
 use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-   #[arg(short, long)]
-   file_name: String,
+    #[arg(short, long)]
+    file_name: String,
 
-   #[arg(long)]
-   file_header: bool,
+    #[arg(long)]
+    file_header: bool,
 
-   #[arg(long)]
-   program_headers: bool,
+    #[arg(long)]
+    program_headers: bool,
 
-   #[arg(long)]
-   section_headers: bool,
+    #[arg(long)]
+    section_headers: bool,
+
+    #[arg(long)]
+    symbols: bool,
+
+    #[arg(long)]
+    dynamic_symbols: bool,
 }
 
 fn main() {
@@ -39,14 +45,43 @@ fn main() {
     }
 
     if args.program_headers {
-        for phdr in elf_file.phdrs {
+        for phdr in &elf_file.phdrs {
             println!("{}", phdr);
         }
     }
 
     if args.section_headers {
-        for s in elf_file.sections {
+        for s in elf_file.sections.iter() {
             println!("{}", s.shdr);
+        }
+    }
+
+    if args.symbols || args.dynamic_symbols {
+        let tables: Option<(elf::symbol::SymbolTable, elf::string_table::StringTable)>;
+        if args.symbols {
+            tables = match elf_file.symbol_table() {
+                Ok(tables) => tables,
+                Err(e) => panic!("Error: {:?}", e),
+            }
+        } else {
+            tables = match elf_file.dynamic_symbol_table() {
+                Ok(tables) => tables,
+                Err(e) => panic!("Error: {:?}", e),
+            }
+        }
+
+        match tables {
+            Some(tables) => {
+                let (symtab, strtab) = tables;
+                for (idx, sym) in symtab.iter().enumerate() {
+                    let name = match strtab.get(sym.name as usize) {
+                        Ok(name) => name,
+                        Err(e) => panic!("Error: {:?}", e),
+                    };
+                    println!("{idx}: {:?} {}", sym, name);
+                }
+            }
+            None => (),
         }
     }
 }
